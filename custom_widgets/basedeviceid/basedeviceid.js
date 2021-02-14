a2b43ae68b74603ba9534d4b0542c54a1f0badc0
Christian Lyra <christian.lyra@rnp.br>
2021-02-14 10:54:50 -0300

    adding the possibility to set deviceid using the widget

--- custom_widgets/basedeviceid/basedeviceid.js
+++ custom_widgets/basedeviceid/basedeviceid.js
@@ -1,10 +1,13 @@
 function basedeviceid(widget_id, url, skin, parameters)
 {   
      // to show this device id on your dashboard, add this widget to a dashboard:
-     //deviceid:
-     //widget_type: deviceid
-     //title: "My id"
-
+     // deviceid:
+     // widget_type: deviceid
+     // title: "My id"
+     
+     // to set the device id, also add:
+    // id: "1234"
+	
     // Will be using "self" throughout for the various flavors of "this"
     // so for consistency ...
 
@@ -14,31 +17,22 @@ function basedeviceid(widget_id, url, skin, parameters)
 
     self.widget_id = widget_id;
 
-    // Store on brightness or fallback to a default
-
-    // Parameters may come in useful later on
-
     self.parameters = parameters;
+
+    //if id was defined, then store it in localstorage
+    if (self.parameters.fields.value)
+    {
+        localStorage['hadasboard-device-id'] = self.parameters.fields.value     
+    }
 
     self.parameters.fields.value = localStorage['hadasboard-device-id']
 
     var callbacks = [];
 
-    // Define callbacks for entities - this model allows a widget to monitor multiple entities if needed
-    // Initial will be called when the dashboard loads and state has been gathered for the entity
-    // Update will be called every time an update occurs for that entity
-
     var monitored_entities =  [];
-
-    // Finally, call the parent constructor to get things moving
 
     WidgetBase.call(self, widget_id, url, skin, parameters, monitored_entities, callbacks);
 
-    // Function Definitions
-
-    // The StateAvailable function will be called when
-    // self.state[<entity>] has valid information for the requested entity
-    // state is the initial state
     // Methods
 
     function OnStateAvailable(self, state)
@@ -54,25 +48,7 @@ function basedeviceid(widget_id, url, skin, parameters)
     function set_value(self, state)
     {
         value = state.state
-
-        if (isNaN(value))
-        {
-            self.set_field(self, "value_style", self.parameters.css.text_style);
-            self.set_field(self, "value", self.map_state(self, value))
-        }
-        else
-        {
-            self.set_field(self, "value_style", self.parameters.css.value_style);
-            self.set_field(self, "value", self.format_number(self, value));
-            self.set_field(self, "unit_style", self.parameters.css.unit_style);
-            if ("units" in self.parameters)
-            {
-                self.set_field(self, "unit", self.parameters.units)
-            }
-            else
-            {
-                self.set_field(self, "unit", state.attributes["unit_of_measurement"])
-            }
-        }
+        self.set_field(self, "value_style", self.parameters.css.text_style);
+        self.set_field(self, "value", value);
     }
 }
